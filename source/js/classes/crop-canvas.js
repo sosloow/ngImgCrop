69047dfddaa7f595ab98f5b0bc07c2d0b0667873
suggim01 <mike.suggitt@bbc.co.uk>
2014-12-08 16:01:08 +0000

    Adding support for rectangle

--- source/js/classes/crop-canvas.js
+++ source/js/classes/crop-canvas.js
@@ -102,8 +102,8 @@ crop.factory('cropCanvas', [function() {
     this.drawCropArea=function(image, centerCoords, size, fnDrawClipPath) {
       var xRatio=image.width/ctx.canvas.width,
           yRatio=image.height/ctx.canvas.height,
-          xLeft=centerCoords[0]-size/2,
-          yTop=centerCoords[1]-size/2;
+          xLeft=size.x,
+          yTop=size.y;
 
       ctx.save();
       ctx.strokeStyle = colors.areaOutline;
@@ -114,8 +114,8 @@ crop.factory('cropCanvas', [function() {
       ctx.clip();
 
       // draw part of original image
-      if (size > 0) {
-          ctx.drawImage(image, xLeft*xRatio, yTop*yRatio, size*xRatio, size*yRatio, xLeft, yTop, size, size);
+      if (size.w > 0 && size.w > 0) {
+          ctx.drawImage(image, xLeft*xRatio, yTop*yRatio, size.w*xRatio, size.h*yRatio, xLeft, yTop, size.w, size.h);
       }
 
       ctx.beginPath();
